c295637f50e4160b89fa41087e9f788cc8044e0c
Lillian <zlillian0@gmail.com>
2020-03-14 14:15:16 +0800

    init commit

---
+++ Dog.java
@@ -0,0 +1,16 @@
+package yyy;
+
+import java.io.Serializable;
+
+public class Dog extends Animal {
+	private String name;
+	
+	public Dog(String name) {
+		super (name);
+		this.name=name;
+	}
+	
+	public void speak() {
+		System.out.println("This is Dog " + name + " speaking.");
+	}
+}
